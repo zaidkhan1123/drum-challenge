4e970b9cd26c10820e33d832b876b72a6253017d
zaidkhan1123 <111523360+zaidkhan1123@users.noreply.github.com>
2022-10-07 02:44:22 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var numberOFdrumbtn = document.querySelectorAll(".drum").length;
+
+for(var i = 0 ; i<numberOFdrumbtn; i++ ){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        
+
+
+    //   let audio = new Audio("./sounds/tom-1.mp3")
+    //   audio.play();
+       });
+     
+     
+}
+
+
+
+
